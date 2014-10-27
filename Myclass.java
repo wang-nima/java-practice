864f264026919fa7ca02ca5b194a8f3e3bab9071
shawn <jak47042007@163.com>
2014-10-26 23:20:43 -0700

    read from txt

---
+++ Myclass.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Myclass {
+
+	public static void main(String[] args) throws IOException {
+		String fileContent = readFileContent("/Users/shiyuwang/java_input.txt");
+		System.out.println(fileContent);
+	}
+
+	// 参数string为你的文件名
+	private static String readFileContent(String fileName) throws IOException {
+
+		File file = new File(fileName);
+
+		BufferedReader bf = new BufferedReader(new FileReader(file));
+
+		String content = "";
+		StringBuilder sb = new StringBuilder();
+
+		while (content != null) {
+			content = bf.readLine();
+
+			if (content == null) {
+				break;
+			}
+
+			sb.append(content);
+		}
+
+		bf.close();
+		return sb.toString();
+	}
+}
